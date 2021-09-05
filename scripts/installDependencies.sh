7cbbffa801edf99e8afcc0a142bf46b440c027ca
Ritesh KT <ktriesh7@gmail.com>
2021-09-05 13:53:07 +0530

    Change install dependencies

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/ubuntu/new-chatapp/venv/bin/activate
+source /home/ubuntu/new_chatapp/venv/bin/activate
 cd /home/ubuntu/new_chatapp
 pip3 install django bcrypt django-extensions
 pip3 install -r requirements.txt
